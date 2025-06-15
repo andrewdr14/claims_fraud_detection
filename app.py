0b95289b0a1a6310b83580fd003bd69f2e9f0a0e
andrewdr14 <andrew.redmond@hotmail.co.uk>
2025-06-15 19:21:03 +0100

    Render port issues - fix attempt

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def download_data():
     return send_file("motor_insurance_claims.csv", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=False)
+    # Dynamically assign Render's port for deployment
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
